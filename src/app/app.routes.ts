d14cd14130582c6653a659d89208e96b7c31991f
Asilbek <asilbekkaromatov2@gmail.com>
2026-06-01 22:42:49 +0500

    components created and navbar added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,27 @@
 import { Routes } from '@angular/router';
+import { Student } from './student/student';
+import { StudentList } from './student-list/student-list';
+import { StudentForm } from './student-form/student-form';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: Student,
+        title: 'Student Management'
+    },
+    {
+        path: 'students',
+        component: StudentList,
+        title: 'Student List'
+    },
+    {
+        path: "create",
+        component: StudentForm,
+        title: 'Create Student'
+    },
+    {
+        path: "edit/:slug",
+        component: StudentForm,
+        title: 'Edit Student'
+    }
+];
