477d81546aad67c8879005416cbd852a72eca9f4
Asilbek <asilbekkaromatov2@gmail.com>
2026-06-03 11:27:07 +0500

    refactor: enhance loading state management in student service and list components

--- src/app/services/student.service.ts
+++ src/app/services/student.service.ts
@@ -1,35 +1,53 @@
 import { inject, Injectable } from '@angular/core';
 import { StudentFormInterface } from '../model/student-form.interface';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, BehaviorSubject } from 'rxjs';
+import { finalize } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
 })
 export class StudentService {
   private http = inject(HttpClient);
-  private apiUrl = 'https://6a1fb7f0e96c1d13b586306b.mockapi.io/';
+  private apiUrl = 'https://6a1fb7f0e96c1d13b586306b.mockapi.io';
+  private loadingSubject = new BehaviorSubject<boolean>(false);
+  loading$ = this.loadingSubject.asObservable();
 
   getStudents(): Observable<StudentFormInterface[]> {
-    return this.http.get<StudentFormInterface[]>(`${this.apiUrl}/students`);
+    this.loadingSubject.next(true);
+    return this.http.get<StudentFormInterface[]>(`${this.apiUrl}/students`).pipe(
+      finalize(() => this.loadingSubject.next(false)),
+    );
   }
 
   getStudentById(id: string | number): Observable<StudentFormInterface> {
-    return this.http.get<StudentFormInterface>(`${this.apiUrl}/students/${id}`);
+    this.loadingSubject.next(true);
+    return this.http.get<StudentFormInterface>(`${this.apiUrl}/students/${id}`).pipe(
+      finalize(() => this.loadingSubject.next(false)),
+    );
   }
 
   addStudent(student: Omit<StudentFormInterface, 'id'>): Observable<StudentFormInterface> {
-    return this.http.post<StudentFormInterface>(`${this.apiUrl}/students`, student);
+    this.loadingSubject.next(true);
+    return this.http.post<StudentFormInterface>(`${this.apiUrl}/students`, student).pipe(
+      finalize(() => this.loadingSubject.next(false)),
+    );
   }
 
   updateStudent(
     id: string | number,
     updatedStudent: StudentFormInterface,
   ): Observable<StudentFormInterface> {
-    return this.http.put<StudentFormInterface>(`${this.apiUrl}/students/${id}`, updatedStudent);
+    this.loadingSubject.next(true);
+    return this.http.put<StudentFormInterface>(`${this.apiUrl}/students/${id}`, updatedStudent).pipe(
+      finalize(() => this.loadingSubject.next(false)),
+    );
   }
 
   deleteStudent(id: string | number): Observable<void> {
-    return this.http.delete<void>(`${this.apiUrl}/students/${id}`);
+    this.loadingSubject.next(true);
+    return this.http.delete<void>(`${this.apiUrl}/students/${id}`).pipe(
+      finalize(() => this.loadingSubject.next(false)),
+    );
   }
 }
--- src/app/student-list/student-list.ts
+++ src/app/student-list/student-list.ts
@@ -1,8 +1,9 @@
 import { StudentService } from './../services/student.service';
-import { ChangeDetectorRef, Component, inject, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, inject, OnInit, OnDestroy } from '@angular/core';
 import { StudentFormInterface } from '../model/student-form.interface';
 import { DatePipe } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-student-list',
@@ -11,13 +12,24 @@ import { RouterLink } from '@angular/router';
   templateUrl: './student-list.html',
   styleUrl: './student-list.css',
 })
-export class StudentList implements OnInit {
+export class StudentList implements OnInit, OnDestroy {
   studentList: StudentFormInterface[] = [];
   studentService = inject(StudentService);
   cdr = inject(ChangeDetectorRef);
+  loading = false;
+  private loadingSub?: Subscription;
 
   ngOnInit(): void {
+    this.loadingSub = this.studentService.loading$.subscribe((l: boolean) => {
+      this.loading = l;
+      this.cdr.detectChanges();
+    });
+
     this.loadStudents();
+  }
+
+  ngOnDestroy(): void {
+    this.loadingSub?.unsubscribe();
   }
 
   loadStudents(){
